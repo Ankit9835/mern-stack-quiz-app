ca943f576dce5bd3ea28cfa3dd9090026d5a43aa
Ankit9835 <ankydchrismatic@gmail.com>
2023-02-21 12:54:20 +0530

    upto ui screen of register and login done

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,22 @@
+import Login from "./pages/common/Login";
+import "./stylesheets/theme.css";
+import "./stylesheets/alignments.css";
+import "./stylesheets/textelements.css";
+import "./stylesheets/custom-components.css";
+import "./stylesheets/form-elements.css";
+import "./stylesheets/layout.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Register from "./pages/common/Register";
 
 function App() {
   return (
-    <div className="App">
-      Quiz Application
-    </div>
+    <BrowserRouter>
+        <Routes>
+          {/* Common Routes */}
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </BrowserRouter>
   );
 }
 
---
+++ client/src/pages/common/Register/index.js
@@ -0,0 +1,45 @@
+import { Form, message } from "antd";
+import React from "react";
+import { useDispatch } from "react-redux";
+import { Link, useNavigate } from "react-router-dom";
+
+
+function Register() {
+ 
+
+  return (
+    <div className="flex justify-center items-center h-screen w-screen bg-primary">
+      <div className="card w-400 p-3 bg-white">
+        <div className="flex flex-col">
+          <h1 className="text-2xl">
+            SHEYQUIZ - REGISTER<i class="ri-user-add-line"></i>
+          </h1>
+          <div className="divider"></div>
+          <Form layout="vertical" className="mt-2">
+            <Form.Item name="name" label="Name">
+              <input type="text" />
+            </Form.Item>
+            <Form.Item name="email" label="Email">
+              <input type="text" />
+            </Form.Item>
+            <Form.Item name="password" label="Password">
+              <input type="password" />
+            </Form.Item>
+
+            <div className="flex flex-col gap-2">
+              <button
+                type="submit"
+                className="primary-contained-btn mt-2 w-100"
+              >
+                Register
+              </button>
+              <Link to="/login">Already a member? Login</Link>
+            </div>
+          </Form>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Register;
